ed91841b9d7e57a33cd544288c081ef334b5f46f
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-07-22 16:01:31 -0500

    finished without styles

--- Ejercicio2/Main.js
+++ Ejercicio2/Main.js
@@ -1,6 +1,11 @@
 let precioCasco = 1250000;
 let precioMonotraje = 2350000;
 let precioGuantes = 750000;
+
+let totalCasco = 0; 
+let totalMonotraje = 0; 
+let totalGuantes = 0; 
+
 let total = 0;
 
 let contadorCasco = 0;
@@ -9,25 +14,29 @@ let contadorGuantes = 0;
 
 
 document.getElementById("casco").addEventListener("click", function(){
-    contadorCasco += 1;
+    // contadorCasco += 1;
+    contadorCasco = parseInt(document.getElementById("cantidadCascos").value);
+    console.log(contadorCasco);
     document.getElementById("totalCasco").innerHTML = (precioCasco * contadorCasco);
-    document.getElementById("cantidadCascos").innerHTML = contadorCasco;
-    total += precioCasco * contadorCasco
+    totalCasco = precioCasco * contadorCasco;
+    total = totalCasco + totalGuantes + totalMonotraje 
     document.getElementById("total").innerHTML = total;
 })
 
 document.getElementById("monotraje").addEventListener("click", function(){
-    contadorMonotraje += 1;
+    // contadorMonotraje += 1;
+    contadorMonotraje = parseInt(document.getElementById("cantidadMonotrajes").value);
     document.getElementById("totalMonotraje").innerHTML = (precioMonotraje * contadorMonotraje);
-    document.getElementById("cantidadMonotrajes").innerHTML = contadorMonotraje;
-    total += precioMonotraje * contadorMonotraje
+    totalMonotraje = precioMonotraje * contadorMonotraje
+    total = totalCasco + totalGuantes + totalMonotraje 
     document.getElementById("total").innerHTML = total;
 })
 document.getElementById("guantes").addEventListener("click", function(){
-    contadorGuantes += 1;
+    // contadorGuantes += 1;
+    contadorGuantes = parseInt(document.getElementById("cantidadGuantes").value);
     document.getElementById("totalGuantes").innerHTML = (precioGuantes * contadorGuantes);
-    document.getElementById("cantidadMonotrajes").innerHTML = contadorMonotraje;
-    total += precioGuantes * contadorGuantes
+    totalGuantes = precioGuantes * contadorGuantes
+    total = totalCasco + totalGuantes + totalMonotraje 
     document.getElementById("total").innerHTML = total;
 })
 
