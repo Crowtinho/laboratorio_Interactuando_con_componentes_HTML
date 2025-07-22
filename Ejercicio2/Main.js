5fe5967d7113498ae5b02f2225279fd75db8242c
Andres Cuervo <andres.cuervo0807@gmail.com>
2025-07-22 17:04:13 -0500

    final commit v1.1.0

--- Ejercicio2/Main.js
+++ Ejercicio2/Main.js
@@ -13,30 +13,41 @@ let contadorMonotraje = 0;
 let contadorGuantes = 0;
 
 
+function formatearCOP(valor) {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0
+  }).format(valor);
+}
+
+
+
 document.getElementById("casco").addEventListener("click", function(){
     // contadorCasco += 1;
     contadorCasco = parseInt(document.getElementById("cantidadCascos").value);
     console.log(contadorCasco);
-    document.getElementById("totalCasco").innerHTML = (precioCasco * contadorCasco);
+    document.getElementById("totalCasco").innerHTML = formatearCOP(precioCasco * contadorCasco);
     totalCasco = precioCasco * contadorCasco;
     total = totalCasco + totalGuantes + totalMonotraje 
-    document.getElementById("total").innerHTML = total;
+    document.getElementById("total").innerHTML = formatearCOP(total);
 })
 
 document.getElementById("monotraje").addEventListener("click", function(){
     // contadorMonotraje += 1;
     contadorMonotraje = parseInt(document.getElementById("cantidadMonotrajes").value);
-    document.getElementById("totalMonotraje").innerHTML = (precioMonotraje * contadorMonotraje);
+    document.getElementById("totalMonotraje").innerHTML = formatearCOP(precioMonotraje * contadorMonotraje);
     totalMonotraje = precioMonotraje * contadorMonotraje
     total = totalCasco + totalGuantes + totalMonotraje 
-    document.getElementById("total").innerHTML = total;
+    document.getElementById("total").innerHTML = formatearCOP(total);
 })
 document.getElementById("guantes").addEventListener("click", function(){
     // contadorGuantes += 1;
     contadorGuantes = parseInt(document.getElementById("cantidadGuantes").value);
-    document.getElementById("totalGuantes").innerHTML = (precioGuantes * contadorGuantes);
+    document.getElementById("totalGuantes").innerHTML = formatearCOP(precioGuantes * contadorGuantes);
     totalGuantes = precioGuantes * contadorGuantes
     total = totalCasco + totalGuantes + totalMonotraje 
-    document.getElementById("total").innerHTML = total;
+    document.getElementById("total").innerHTML = formatearCOP(total);
 })
+
 
